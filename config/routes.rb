195ba00fccc0e025e4712eb3991b4b78ed5e39f4
EC2 Default User <ec2-user@ip-172-31-45-194.ap-northeast-1.compute.internal>
2020-10-07 11:05:15 +0000

    make controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index' => "posts#index"
+  get 'users/index' => "users#index"
+  get '/' => "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

87cf7bb3e2d9cbd414470b2a52684ae801c74e9e
EC2 Default User <ec2-user@ip-172-31-45-194.ap-northeast-1.compute.internal>
2020-10-08 02:05:13 +0000

    pre-login_system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'posts/index' => "posts#index"
-  get 'users/index' => "users#index"
-  get '/' => "home#top"
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/index" => "users#index"
+  get "users/:id" => "users#show"
+  post "login" => "users#login"
+  post "logout" =>"users#logout"  
+  get '/' => "users#login_form"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
